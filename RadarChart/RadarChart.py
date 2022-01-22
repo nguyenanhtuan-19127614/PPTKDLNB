2a940a5905a493917bb9530d69c30368bd2e6939
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-01-22 20:32:14 +0700

    radarchart

---
+++ RadarChart/RadarChart.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+
+# read dataset from csv and perform preprocessing
+data = pd.read_csv('data.csv')
+
+categories = ['SE','AI','OOP','MSA',
+            'OS','DHMT','XLAV','VLDC2']
+# plot unfilled scatter plot
+fig = go.Figure()
+fig.add_trace(go.Scatterpolar(
+    r=pd.Series(data.loc[0].values),
+      theta=categories,
+      fill='toself',
+      name='Student A'
+))
+fig.add_trace(go.Scatterpolar(
+    r=pd.Series(data.loc[1].values),
+      theta=categories,
+      fill='toself',
+      name='Student B'
+))
+fig.update_layout(
+    polar=dict(
+        radialaxis=dict(
+            visible=True,
+        ),
+    ),
+    template='plotly_dark',
+    showlegend=True,
+
+)
+fig.show()
