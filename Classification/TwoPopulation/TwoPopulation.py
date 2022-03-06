3bf7947d015039642ea5870619892ad8c1fcdb7c
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-03-06 20:44:48 +0700

    comment, add folder

--- Classification/TwoPopulation/TwoPopulation.py
+++ Classification/TwoPopulation/TwoPopulation.py
@@ -7,7 +7,7 @@ import pandas
 data=pandas.read_csv("SalmonData.csv")
 X_1=np.vstack((data["Freshwater_Alask"],data["Marine_Alask"])).T
 X_2=np.vstack((data["Freshwater_Canadian"],data["Marine_Canadian"])).T
-print(X_2)
+
 n_1=X_1.shape[0]
 n_2=X_2.shape[0]
 p_1=X_1.shape[1]
@@ -22,15 +22,18 @@ S_2=np.cov(X_2.T)
 
 # Tính S_pooled
 S_pooled=(1/(n_1+n_2-2))*((n_1-1)*S_1+(n_2-1)*S_2)
+# S_pooled nghịch đảo
 S_pooled_inv=np.linalg.inv(S_pooled)
+# a= chuyển vị (mean vector x1 - mean vector x2) X S_pooled nghịch đảo
 a=(mean_X_1-mean_X_2).T.dot(S_pooled_inv)
-w=a.dot(mean_X_1-mean_X_2)*0.5
+#Tính m mũ
+m=0.5*a.dot(mean_X_1-mean_X_2)
 
-def classification_X(X,a,w):
+def classification_X(X,a,m):
     class_id=[]
     for i in X:
-        print(w)
-        if a.dot(i.T) >= w:
+        print(a.dot(i.T))
+        if a.dot(i.T) >= m:
             class_id.append("Alask")
         else:
             class_id.append("Canadian")
@@ -40,7 +43,7 @@ data_test=pandas.read_csv("Test.csv")
 X_test=np.vstack((data_test["Freshwater"],data_test["Marine"])).T
 y_test=data_test["Label"]
 
-y_predict= classification_X (X_test,a,w)
+y_predict= classification_X (X_test,a,m)
 print(y_predict)
 accuracy_score_test= accuracy_score(y_predict,y_test)
 print("accuracy score test: ",accuracy_score_test*100)
@@ -54,10 +57,13 @@ plt.show()
 
 temp_1=a.dot(X_1.T).reshape(-1,1)
 temp_2=a.dot(X_2.T).reshape(-1,1)
-plt.plot(temp_1,"o",color="blue",label="1")
-plt.plot(temp_2,"o",color="red",label="0")
-xx = np.arange(-2, 50, 0.01).reshape(-1, 1)
-y=w+xx*0
+
+plt.plot(temp_1,"o",color="blue",label="Alask")
+plt.plot(temp_2,"o",color="red",label="Canadian")
+xx = np.arange(-2, 50, 0.1).reshape(-1, 1)
+
+y=m+xx*0
+
 plt.plot(xx,y,color="black")
 plt.legend()
 plt.show()
