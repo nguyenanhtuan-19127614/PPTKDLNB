3c0e02f8141e095f94f53301e18aab7366182a22
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-03-07 23:20:25 +0700

    add label matplotlib

--- Classification/TwoPopulation/TwoPopulation.py
+++ Classification/TwoPopulation/TwoPopulation.py
@@ -52,6 +52,8 @@ print("accuracy score test: ",accuracy_score_test*100)
 plt.plot(X_1[:,0], X_1[:,1], "o", color="blue",label="Alask")
 plt.plot(X_2[:,0], X_2[:,1], "o", color="red",label="Canadian")
 plt.title("Data")
+plt.xlabel("Freshwater")
+plt.ylabel("Marine")
 plt.legend()
 plt.show()
 
@@ -59,12 +61,14 @@ plt.show()
 #Tính y của X_1 và X_2
 y_1=a.dot(X_1.T).reshape(-1,1)
 y_2=a.dot(X_2.T).reshape(-1,1)
+
 plt.plot(y_1,"o",color="blue",label="Alask")
 plt.plot(y_2,"o",color="red",label="Canadian")
-xx = np.arange(-2, 50, 0.1).reshape(-1, 1)
+xx = np.arange(0, 50, 0.1).reshape(-1, 1)
 m_new=m+xx*0 #m_new vẫn là m nhưng nhân với xx để tạo array vẽ cho dễ
 plt.plot(xx,m_new,color="black")
 plt.title("Classification Model")
+
 plt.legend()
 plt.show()
 
@@ -75,6 +79,8 @@ class_Canadian = X_test[y_test=="Canadian"]
 plt.plot(class_Alask[:,0], class_Alask[:,1], "o", color="blue",label="Alask")
 plt.plot(class_Canadian[:,0], class_Canadian[:,1], "o", color="red",label="Canadian")
 plt.title("Data Test")
+plt.xlabel("Freshwater")
+plt.ylabel("Marine")
 plt.legend()
 plt.show()
 
@@ -85,9 +91,10 @@ y_Canadian=a.dot(class_Canadian.T).reshape(-1,1)
 
 plt.plot(y_Alask,"o",color="blue",label="Alask")
 plt.plot(y_Canadian,"o",color="red",label="Canadian")
-xx = np.arange(-2, 50, 0.1).reshape(-1, 1)
+xx = np.arange(0, 10, 0.1).reshape(-1, 1)
 m_new=m+xx*0
 plt.plot(xx,m_new,color="black")
 plt.title("Data Test Classification Model")
+
 plt.legend()
 plt.show()
