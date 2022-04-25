e63685531f63b7753df1831d2c964ea28b6897b8
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-04-25 15:54:05 +0700

    datavisulize + pcavslda - lab3

---
+++ PRACTICE_03/VisualizeData.py
@@ -0,0 +1,18 @@
+import numpy as np
+from matplotlib import pyplot
+import pandas as pd
+from pandas.plotting import scatter_matrix
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pd.read_csv(url, names=names)
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+# histograms
+dataset.hist()
+pyplot.show()
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
